ef9e2b5b56572d7a1c2297711623cfae744d68a5
ovalriver23 <tuanaak45@gmail.com>
2026-05-11 11:17:39 +0300

    fix the yml file

--- inventree-material-check/setup.sh
+++ inventree-material-check/setup.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 set -e
 
-echo "InvenTree kurulumu başlıyor..."
-
-echo "[1/3] Servisler başlatılıyor (migrate + admin kurulumu dahil)..."
+echo "InvenTree başlatılıyor..."
 docker compose up -d
-
-echo "[2/3] Setup tamamlanması bekleniyor..."
-docker compose wait setup
-
-echo "[3/3] Server ve worker yeniden başlatılıyor..."
-docker compose restart server worker
 
 echo ""
 echo "Kurulum tamamlandı!"
-echo "Adres : http://localhost:8000"
+echo "Adres     : http://localhost:8000"
 echo "Kullanıcı : admin"
-echo "Şifre    : admin1234"
+echo "Şifre     : admin1234"
